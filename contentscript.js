59769a6c896e4322fa406343fd4861218c69a73f
Chandramani Vyas <chandramani09022001@gmail.com>
2022-11-18 23:00:53 +0530

    Aromal make us proud

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,61 @@
-console.log("Content Script");
-
 window.onclick = e => {
-    console.log(e.target);  // to get the element
-    console.log(e.target.tagName);  // to get the element tag name alone
+    var count = 0;
+    var value;
+    
+    if((e.target.tagName==="BUTTON")){
+    value=e.target.getAttribute("id").slice(4,5);
+    var x = "card" + value + "-img";
+    console.log(document.getElementById(x));
+    if(value==="1")
+    {
+        count++;
+        
+    }
+    else if(value==="2"){
+        count++
+    }
+    else if(value==="3"){
+        count++
+    }
+    else if(value==="4"){
+        count++
+    }
+    else if(value==="5"){
+        count++
+    }
+    else if(value==="6"){
+        count++
+    } 
 }
+  
+}
+
+
+
+
+// firebase 
+/*const firebaseConfig = {
+    apiKey: "AIzaSyB8v7HnmF2pttQcp5B1AvJ4N1Dd5mAoQew",
+    authDomain: "kaizen-f0bb3.firebaseapp.com",
+    databaseURL: "https://kaizen-f0bb3-default-rtdb.firebaseio.com",
+    projectId: "kaizen-f0bb3",
+    storageBucket: "kaizen-f0bb3.appspot.com",
+    messagingSenderId: "730539601182",
+    appId: "1:730539601182:web:78acbf561524a798cb2d0f"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+var userEventDB = firebase.database().ref("userEvent");
+
+const sendUserData = (id, title, count) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+    id: id,
+    title: title,
+    count: count,
+  });
+};*/
